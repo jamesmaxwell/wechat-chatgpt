ef6b85f5d1c3c4c735cec5029ee753b4ed4ede32
James Maxwell <maxwell_hu@163.com>
2023-03-20 21:36:07 +0800

    fix: puppet wechat 问题

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { WechatyBuilder } from "wechaty";
 import QRCode from "qrcode";
 import { ChatGPTBot } from "./bot.js";
-import {config} from "./config.js";
+import { config } from "./config.js";
 const chatGPTBot = new ChatGPTBot();
 
-const bot =  WechatyBuilder.build({
+const bot = WechatyBuilder.build({
   name: "wechat-assistant", // generate xxxx.memory-card.json and save login data for the next login
+  puppet: "wechaty-puppet-wechat",
+  puppetOptions: {
+    uos: true,
+  },
 });
 async function main() {
-  const initializedAt = Date.now()
+  const initializedAt = Date.now();
   bot
     .on("scan", async (qrcode, status) => {
       const url = `https://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`;
@@ -21,8 +25,12 @@ async function main() {
       chatGPTBot.setBotName(user.name());
       console.log(`User ${user} logged in`);
       console.log(`私聊触发关键词: ${config.chatPrivateTriggerKeyword}`);
-      console.log(`已设置 ${config.blockWords.length} 个聊天关键词屏蔽. ${config.blockWords}`);
-      console.log(`已设置 ${config.chatgptBlockWords.length} 个ChatGPT回复关键词屏蔽. ${config.chatgptBlockWords}`);
+      console.log(
+        `已设置 ${config.blockWords.length} 个聊天关键词屏蔽. ${config.blockWords}`
+      );
+      console.log(
+        `已设置 ${config.chatgptBlockWords.length} 个ChatGPT回复关键词屏蔽. ${config.chatgptBlockWords}`
+      );
     })
     .on("message", async (message) => {
       if (message.date().getTime() < initializedAt) {
